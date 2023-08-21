a2dcd440241d112b4283e12cd6427e43c0ba93c6
Alex Kharlamov <aakharlam@gmail.com>
2023-08-21 14:39:52 +0400

    init: created project

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+import path from "path";
+
+import compression from "compression";
+import dotenv from "dotenv";
+import express from "express";
+
+// import errorHandler from "./controllers/errorController/errorController.js";
+import connectDB from "./middleware/db.js";
+import expressHelpers from "./middleware/expressHelpers.js";
+import sanitization from "./middleware/sanitization.js";
+import security from "./middleware/security.js";
+// import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+security(app);
+expressHelpers(app);
+sanitization(app);
+app.use(compression());
+
+// MAIN REQUEST ROUTES
+
+//
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
+connectDB().then(() => {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`🟢 Server is running on port ${port}`);
+  });
+});
