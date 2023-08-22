486eaed559b0b6ec2276aca642d0e92a4349ca8a
Alex Kharlamov <aakharlam@gmail.com>
2023-08-22 10:47:42 +0400

    fix: ErrorPage import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ErrorPage from "./pages/errorPage";
+import ErrorPage from "./pages/ErrorPage";
 import App from "./App";
 import "./index.css";
 import {
