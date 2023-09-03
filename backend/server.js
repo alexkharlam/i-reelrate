d6c25fc744644cec8c204c9a50ad87f5cf4d8eeb
Alex Kharlamov <aakharlam@gmail.com>
2023-09-03 18:38:08 +0400

    feat: Open website in dev mode

--- backend/server.js
+++ backend/server.js
@@ -24,18 +24,18 @@ app.use(compression());
 
 app.use("/auth", authRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("Hello, world! In development");
-  });
-}
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+);
+
+// else {
+//   app.get("/", (req, res) => {
+//     res.send("Hello, world! In development");
+//   });
+// }
 
 app.use(errorHandler);
 
