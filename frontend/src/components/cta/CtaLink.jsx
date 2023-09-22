dc38e6512e897c2eab321d706b9d164dea3952de
Alex Kharlamov <aakharlam@gmail.com>
2023-09-22 16:34:04 +0400

    fix: margins

--- frontend/src/components/cta/CtaLink.jsx
+++ frontend/src/components/cta/CtaLink.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function CtaLink({ text, to }) {
   return (
     <Link
-      className="block hover:scale-[101%] mb-4 md:p-4 p-3 rounded-lg transition-all duration-300 hover:bg-primary-700 text-center text-1xl font-semibold bg-primary-600 text-white"
+      className="block hover:scale-[101%] mb-3.5 md:p-4 p-3 rounded-lg transition-all duration-300 hover:bg-primary-700 text-center text-1xl font-semibold bg-primary-600 text-white"
       to={to}
     >
       {text}
--- frontend/src/components/reviewLists/PopularReviews.jsx
+++ frontend/src/components/reviewLists/PopularReviews.jsx
@@ -6,7 +6,7 @@ function PopularReviews() {
   const { isLoading, reviews } = usePopularReviews();
 
   return (
-    <div className="mb-3">
+    <div className="mb-3.5">
       <h3 className="text-2xl font-semibold">Popular reviews</h3>
       {isLoading && <LoadingSpinner />}
       {!isLoading && reviews.length > 0 && <ReviewList reviews={reviews} />}
