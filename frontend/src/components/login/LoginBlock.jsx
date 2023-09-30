fabb39dd1875aa305197ffbc674f6031b8e4c504
Alex Kharlamov <aakharlam@gmail.com>
2023-09-30 19:06:56 +0400

    Update LoginBlock.jsx

--- frontend/src/components/login/LoginBlock.jsx
+++ frontend/src/components/login/LoginBlock.jsx
@@ -5,7 +5,7 @@ function LoginBlock({ xl = false }) {
   const { t } = useTranslation();
 
   return (
-    <div className="p-3 rounded-lg bg-gradient-to-r from-[#4b6cb7] to-[#182848] flex flex-col items-center text-white">
+    <div className="p-3 rounded-lg bg-gradient-to-r from-[#4b6cb7] to-[#182848] flex flex-col mb-4 items-center text-white">
       <h3 className="font-bold text-2xl md:text-4xl mb-3.5">
         {t("login.title")}
       </h3>
